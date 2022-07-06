a572d17c723ba102fcf5787d53b7eef3e7b5ed73
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-06 18:15:03 +0300

    book

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Books.css';
 import Forms from './Forms';
+import Book from './Book';
 
 class Books extends Component {
   constructor(props) {
@@ -11,27 +12,7 @@ class Books extends Component {
   render() {
     return (
       <>
-        <div className="card">
-          <div className="main-area">
-            <span>Action</span>
-            <h1>The hunger games</h1>
-            <p>Unseen Content</p>
-            <ul>
-              <li>Corn</li>
-              <li>Corn</li>
-              <li>Corn</li>
-            </ul>
-          </div>
-
-          <div className="main-area2">
-            <h4>64%</h4>
-          </div>
-          <div className="main-area3">
-            <h4>Current Chapter</h4>
-            <h5>Chapter 17</h5>
-            <button type="button">UPDATE CHAPTER</button>
-          </div>
-        </div>
+        <Book />
         <Forms />
       </>
     );
