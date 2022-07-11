5f86f039035976882b83efc1846ff806bc4a1105
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-11 14:32:16 +0300

    navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,40 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import './Books.css';
+import { FaUserCircle } from 'react-icons/fa';
 
-function Navbar() {
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Books',
+    },
+    {
+      id: 2,
+      path: '/categories',
+      text: 'Categories',
+    },
+  ];
+
   return (
-    <nav>
-      <ul className="nav-content">
-        <li>Bookstore CMS</li>
-        <li><Link to="/">BOOKS</Link></li>
-        <li><Link to="/Category">CATEGORY</Link></li>
-      </ul>
+    <nav className="Navbar">
+      <div className="Nav1">
+        <header>
+          <h1>Bookstore CMS</h1>
+        </header>
+        <ul className="Links">
+          {links.map((link) => (
+            <li key={link.id}>
+              <Link to={link.path}>
+                {link.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <FaUserCircle />
     </nav>
   );
-}
+};
+
 export default Navbar;
