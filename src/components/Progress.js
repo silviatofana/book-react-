27bb321d3946386674c2b69a1ce6158b175150dc
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-11 14:34:32 +0300

    Progress.js

---
+++ src/components/Progress.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Progress = () => (
+  <div>
+    <p>CURRENT CHAPTER</p>
+    <p>Introduction</p>
+    <button type="button">UPDATE PROGRESS</button>
+  </div>
+);
+
+export default Progress;
