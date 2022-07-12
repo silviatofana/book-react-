175c4937f93883495e2cefa581c575889a5c0ae3
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-12 13:10:52 +0300

    Update books.js/books/redux

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,60 +1,34 @@
 import { v4 as uuidv4 } from 'uuid';
 
-const ADD_BOOK = '/react-bookstore/books/addBook';
-const REMOVE_BOOK = '/react-bookstore/books/removeBook';
-const initialState = [{
-  id: uuidv4(),
-  title: 'Book1 Title',
-  author: 'Book1 Author',
-  category: 'Fiction',
-  progress: 70,
-  currentChapter: 'chapter 4',
-},
-{
-  id: uuidv4(),
-  title: 'Book2 Title',
-  category: 'Religion',
-  author: 'Book2 Author',
-  progress: 20,
-  currentChapter: 'chapter 4',
-},
-{
-  id: uuidv4(),
-  title: 'Book3 Title',
-  author: 'Book3 Author',
-  category: 'Romance',
-  progress: 82,
-  currentChapter: 'chapter 5',
-}];
+const addBook = '/react-bookstore/books/addBook';
+const removeBook = '/react-bookstore/books/removeBook';
+const initialState = [];
 
 const reduceBook = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_BOOK:
-      return [...state, action.payload];
-    case REMOVE_BOOK:
-      return [...state.filter((book) => book.id !== action.payload.bookid)];
+    case addBook:
+      return [...state, action.info];
+    case removeBook:
+      return [...state.filter((book) => book.title !== action.info.title)];
     default:
       return state;
   }
 };
 
-const addingBook = (newBook) => ({
-  type: ADD_BOOK,
-  payload: {
-    title: newBook.title,
-    author: newBook.author,
+const addingBook = () => ({
+  type: addBook,
+  info: {
+    title: 'new book',
+    author: 'new author',
     id: uuidv4(),
-    category: newBook.category,
-    progress: Math.floor(Math.random() * 100),
-    currentChapter: `Chapter ${Math.floor(Math.random() * 15)}`,
   },
 });
 
-const removingBook = (id) => ({
-  type: REMOVE_BOOK,
-  payload: {
-    bookid: id,
+const removingBook = () => ({
+  type: removeBook,
+  info: {
+    title: 'new book',
   },
 });
-export { addingBook, removingBook };
 export default reduceBook;
+export { addingBook, removingBook };
