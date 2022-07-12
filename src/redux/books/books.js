65cc12aa48b5798386fb0af9a354b53343e2978e
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-12 16:55:13 +0300

    Update books.js/books/redux

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,34 +1,56 @@
-import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
-const addBook = '/react-bookstore/books/addBook';
-const removeBook = '/react-bookstore/books/removeBook';
+const ADD_BOOK = '/react-bookstore/books/addBook';
+const REMOVE_BOOK = '/react-bookstore/books/removeBook';
+const getBookUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/48yLRiaTQ7Hf0y7r4Sqa/books';
 const initialState = [];
+
+const addingBook = (newBook) => ({
+  type: ADD_BOOK,
+  payload: newBook,
+});
+
+// eslint-disable-next-line camelcase
+const removingBook = (item_id) => ({
+  type: REMOVE_BOOK,
+  payload: {
+    // eslint-disable-next-line camelcase
+    bookid: item_id,
+  },
+});
+
+const fetchBooks = () => async (dispatch) => {
+  await axios.get(getBookUrl).then((response) => dispatch(addingBook(response.data)));
+};
+
+const postBook = (addingBook) => async (dispatch) => {
+  await axios.post(getBookUrl, addingBook).then(() => dispatch(fetchBooks()));
+};
+
+// eslint-disable-next-line camelcase
+const deleteBook = (id) => async (dispatch) => {
+  await axios.delete(`${getBookUrl}/${id}`).then(() => dispatch(fetchBooks()));
+};
 
 const reduceBook = (state = initialState, action) => {
   switch (action.type) {
-    case addBook:
-      return [...state, action.info];
-    case removeBook:
-      return [...state.filter((book) => book.title !== action.info.title)];
+    case ADD_BOOK: {
+      const books = Object.entries(action.payload);
+      console.log(books);
+      return books.map((book) => ({
+        id: book[0],
+        ...book[1][0],
+      }));
+    }
+    case REMOVE_BOOK:
+      return [...state.filter((book) => book.id !== action.payload.bookid)];
     default:
       return state;
   }
 };
 
-const addingBook = () => ({
-  type: addBook,
-  info: {
-    title: 'new book',
-    author: 'new author',
-    id: uuidv4(),
-  },
-});
-
-const removingBook = () => ({
-  type: removeBook,
-  info: {
-    title: 'new book',
-  },
-});
+export {
+  addingBook, removingBook, postBook, fetchBooks, deleteBook,
+};
 export default reduceBook;
-export { addingBook, removingBook };
+
