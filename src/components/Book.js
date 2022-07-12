82f865c1cba94d412d663aa827268e7750cca235
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-12 14:29:42 +0300

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,40 +1,55 @@
-import React, { Component } from 'react';
-import './Books.css';
-import Forms from './Forms';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import { deleteBook } from '../redux/books/books';
+import 'react-circular-progressbar/dist/styles.css';
 
-class Books extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Book = ({
+  id, title, author, genre, progress,
+}) => {
+  const dispatch = useDispatch();
 
-  render() {
-    return (
-      <>
-        <div className="card">
-          <div className="main-area">
-            <span>Action</span>
-            <h1>The hunger games</h1>
-            <p>Unseen Content</p>
-            <ul>
-              <li>Corn</li>
-              <li>Corn</li>
-              <li>Corn</li>
-            </ul>
-          </div>
-
-          <div className="main-area2">
-            <h4>64%</h4>
-          </div>
-          <div className="main-area3">
-            <h4>Current Chapter</h4>
-            <h5>Chapter 17</h5>
-            <button type="button">UPDATE CHAPTER</button>
-          </div>
+  return (
+    <div className="book-card">
+      <div className="book-left-section">
+        <p className="book-genre">{genre}</p>
+        <p className="book-title">{title}</p>
+        <p className="book-author">{author}</p>
+        <ul className="book-btns">
+          <li>Comments</li>
+          <div className="line-vertical" />
+          <li><button className="remove-btn" type="button" onClick={() => dispatch(deleteBook(id))}>Remove</button></li>
+          <div className="line-vertical" />
+          <li>Edit</li>
+        </ul>
+      </div>
+      <div className="book-middle-section">
+        <div className="progressbar-container">
+          <CircularProgressbar value={progress} />
         </div>
-        <Forms />
-      </>
-    );
-  }
-}
-export default Books;
+        <div>
+          <p>{`${Math.floor(Math.random() * 100)}%`}</p>
+          <p>Completed</p>
+        </div>
+      </div>
+      <div className="book-right-section">
+        <p>CURRENT CHAPTER</p>
+        <p>
+          Chapter
+          {` ${Math.floor(Math.random() * 10)}`}
+        </p>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
+    </div>
+  );
+};
+
+Book.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  progress: PropTypes.number.isRequired,
+};
+export default Book;
